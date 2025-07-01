fb7b48ca09b1a0ec730b80f9c29dc36c1f44cf72
Aaron Crawford <acraw43@protonmail.com>
2025-06-30 21:13:18 -0700

    Implemented pot file to support translations.

---
+++ package_extension.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+gnome-extensions pack pipewire-airplay-toggle@craw0967.github.com \
+    --force \
+    --extra-source=constants \
+    --extra-source=functions \
+    --extra-source=icons \
+    --extra-source=classes \
+    --extra-source=../LICENSE
