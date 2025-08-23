647a12551d9091efb2618441b2063e9adcaa138e
Aaron Crawford <acraw43@protonmail.com>
2025-08-22 21:29:35 -0700

    Added script to package extension for upload

---
+++ package_extension.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+gnome-extensions pack pipewire-airplay-toggle@craw0967.github.com \
+    --force \
+    --extra-source=constants \
+    --extra-source=functions \
+    --extra-source=icons \
+    --extra-source=../LICENSE
